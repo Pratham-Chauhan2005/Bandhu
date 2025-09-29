15c0c7271a6d4a0ff677bb7e5f4631ecd5faaa89
fangster <thefunster05@gmail.com>
2025-09-29 09:01:28 +0000

    try again

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -4,14 +4,7 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_AUTH_DOMAIN",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_STORAGE_BUCKET",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "YOUR_APP_ID"
-};
+const firebaseConfig = {"apiKey":"fake-api-key","authDomain":"fake-auth-domain","projectId":"fake-project-id","storageBucket":"fake-storage-bucket","messagingSenderId":"fake-messaging-sender-id","appId":"fake-app-id"};
 
 // Initialize Firebase
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
